df2f323fa7b9775c20ae173ec12c1c9205e61de6
bastazini <bastazini.vinicius@gmail.com>
2022-02-23 20:36:52 +0000

    Add files via upload

---
+++ analise de rede.R
@@ -0,0 +1,62 @@
+###packages
+require(bipartite)
+require(igraph)
+require(influential)
+require(vegan)
+
+rm(list=ls())
+###Import data
+#Load data
+rede=read.table(pipe("pbpaste"), sep="\t", header=T,row.names=1);rede
+dim(rede)
+
+##Calculate habitat diversity using equivalent numbers 
+habitat=read.table(pipe("pbpaste"), sep="\t", header=T);habitat
+eq.numbers=exp(vegan::diversity(habitat, "shannon")); eq.numbers 
+##copy and paste to excel, to organize data
+clipr::write_clip(eq.numbers)
+
+
+#### Calcularing IVI
+My_graph <- graph_from_incidence_matrix(rede,weighted = TRUE)
+Graph_IVI <- ivi(graph = My_graph, mode = "all")
+ivi=Graph_IVI[1:48]
+clipr::write_clip(ivi.aves)
+cent_network.vis(graph = My_graph, cent.metric = Graph_IVI,
+                 legend.title = "Valor Integrado de Influencia",
+                 plot.title = "", layout= "kk", dist.power=2, legend.position="right", boxed.legend=FALSE, show.labels=FALSE, 
+                 node.shape=c("circle", "circle", "circle", "circle", "circle", "circle", 
+                              "circle", "circle", "circle", "circle", "circle", "circle", "circle", 
+                              "circle", "circle", "circle", "circle", "circle", "circle", "circle", 
+                              "circle", "circle", "circle", "circle", "circle", "circle", "circle", 
+                              "circle", "circle", "circle", "circle", "circle", "circle", "circle", 
+                              "circle", "circle", "circle", "circle", "circle", "circle", "circle", 
+                              "circle", "circle", "circle", "circle", "circle", "circle", "circle", 
+                              "square", "square", "square", "square", "square", "square", "square", 
+                              "square", "square", "square", "square", "square", "square", "square", 
+                              "square", "square", "square", "square", "square", "square", "square", 
+                              "square", "square", "square", "square", "square", "square", "square", 
+                              "square", "square", "square", "square", "square", "square", "square", 
+                              "square", "square", "square"))
+               
+                 ###Order IVI
+#negative sign serves as a trick to rank IVI on descending order
+ivi.order=rank(-ivi);ivi.order
+edit(ivi.order)
+
+###Robustness
+##specify "ext.row" based on the IVI rank vector
+rob =second.extinct(rede, participant="lower", method="external",ext.row= c(14, 39.5, 13, 5, 33, 27, 41, 10, 9, 1, 22, 37, 25, 
+                                                                               7, 26, 38, 21, 39.5, 35.5, 28, 24, 42.5, 42.5, 18, 35.5, 4, 20, 
+                                                                               47.5, 45, 29, 44, 47.5, 15, 46, 32, 23, 30, 2, 8, 19, 6, 17, 
+                                                                           11, 31, 3, 34, 12, 16))
+rob.random=  second.extinct(rede, participant="lower", method="random", nrep=1000,details=FALSE)  
+###Robustness
+robustness(rob)
+robustness(rob.random)
+### this fucntion plots the ATC and calculates its exponet
+###This is a modification from slope.bipartite taken from Bastazini et al. 2019. Environmental Conservation 46.1 (2019): 52-58.
+source("fit.hyperbolic.R")
+par(mfrow=c(1,2))
+fit.hyperbolica(rob)
+fit.hyperbolica(rob.random)
---
+++ fit.hyperbolic.R
@@ -0,0 +1,27 @@
+fit.hyperbolica=function (object, plot.it = TRUE, ...) 
+{
+  if (class(object) != "bipartite") 
+    stop("This function cannot be meaningfully applied to objects of this class.")
+  N <- colSums(object)
+  if (all(object[-nrow(object), 2] == 1)) 
+    y <- -object[, 3]
+  else y <- -object[, 2]
+  y <- (sum(y) - cumsum(y))/sum(y)
+  x <- (object[, "no"]/max(object[, "no"]))
+  fit <- try(nls(y ~ 1 - x^a, start = list(a = 1)))
+  if (class(fit) == "try-error") 
+    fit <- nls((y + rnorm(length(y), s = 0.01)) ~ 1 - x^a, 
+               start = list(a = 1))
+  if (plot.it) {
+    par(mar = c(5, 5, 1, 1))
+    plot(x, y, xlab = "Fração de manchas eliminadas", 
+         ylab = "Fração de espécies de aves sobreviventes", 
+         axes = TRUE, type = "n", cex.lab = 1)
+    
+    
+    points(x, y, pch=16, ...)
+    lines(seq(0, 1, 0.1), predict(fit, newdata = data.frame(x = seq(0, 
+                                                                    1, 0.1))), col = "red", lwd = 2)
+  }
+  return(c(exponent = as.numeric(coef(fit)[1])))
+}
