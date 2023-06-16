f009637f9ad9101a92ac1496f6e3164fcda36eca
bastazini <bastazini.vinicius@gmail.com>
2023-06-16 12:18:55 +0100

    Add files via upload

--- lms figura final.R
+++ lms figura final.R
@@ -9,10 +9,10 @@ names(dados)
 dados1=as.data.frame(scale(dados))
 mod=lm(IVI ~  Habitat.Diversity+ Distance.Nearest.Patch+ Area, data=dados1)
 
-plot1=plot_model(mod, colors="bw", show.values=TRUE, title= "Juncais", axis.title ="")
+plot1=plot_model(mod, colors="bw", show.values=TRUE, title= "Juncais", axis.title ="", vline.color= "red")
 plot1 <- plot1 + theme(axis.title.y = element_blank(), axis.text.y = element_blank(), axis.ticks.y = element_blank())
 
-plot2=plot_model(mod, colors="bw", show.values=TRUE, title= "Sangradouros", axis.title ="")
+plot2=plot_model(mod, colors="bw", show.values=TRUE, title= "Sangradouros", axis.title ="",vline.color= "red")
 
 plot2 <- plot2 + theme(axis.title.y = element_blank(), axis.text.y = element_blank(), axis.ticks.y = element_blank())
 
